3d0f61d39da2dbbdcb182a44ea694863b3d3297c
Mike Provence <49548172+M-PROV@users.noreply.github.com>
2024-02-28 15:54:49 -0800

    Created initial layout with webpack deependencies. Plans to add logic.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: '[name][ext]',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: 'src/index.html', // Path to HTML template
+        filename: 'index.html', // Output file name
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
